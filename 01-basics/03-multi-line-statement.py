1cbfc583f12c8ac217e91cd400212754a92c62e9
Rick Beerendonk <rick@beerendonk.com>
2020-07-08 09:46:36 +0200

    Add clarifying comment.

--- 01-basics/03-multi-line-statement.py
+++ 01-basics/03-multi-line-statement.py
@@ -3,6 +3,8 @@
 
 # Normally statements are single line.
 # Use a \ to let the statement continue on the next line.
+#
+# Multiline expressions in parentheses, square brackets or curly braces don't need backslashes.
 
 print(\
     "test")

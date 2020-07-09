f9baa9d09c690b00275ae811b9505410bb74425e
Rick Beerendonk <rick@beerendonk.com>
2020-07-09 10:15:23 +0200

    Add keywords.

---
+++ 01-basics/04-keywords.py
@@ -0,0 +1,10 @@
+# European Union Public License version 1.2
+# Copyright © 2020 Rick Beerendonk
+
+# False      await      else       import     pass
+# None       break      except     in         raise
+# True       class      finally    is         return
+# and        continue   for        lambda     try
+# as         def        from       nonlocal   while
+# assert     del        global     not        with
+# async      elif       if         or         yield

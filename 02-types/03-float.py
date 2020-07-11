4844dfd20908ede41dc1d3da2a96740be50f0b4e
Rick Beerendonk <rick@beerendonk.com>
2020-07-11 22:09:42 +0200

    Add "Float" example.

---
+++ 02-types/03-float.py
@@ -0,0 +1,13 @@
+# European Union Public License version 1.2
+# Copyright © 2020 Rick Beerendonk
+
+print(type(0.0))
+print(type(1.1))
+print(type(1.))
+print(type(.1))
+print(type(0e0))
+print(type(1e100))
+print(type(3.14e-10))
+
+print(type(3.141_593))
+print(type(3.14e-100_000))

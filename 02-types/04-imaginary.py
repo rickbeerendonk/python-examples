4c47e3e64212d5ba10812a1ae6397544c51e5b83
Rick Beerendonk <rick@beerendonk.com>
2020-07-12 22:19:08 +0200

    Add "imaginary" example.

---
+++ 02-types/04-imaginary.py
@@ -0,0 +1,18 @@
+# European Union Public License version 1.2
+# Copyright © 2020 Rick Beerendonk
+
+# Imaginary part of a complex number (real part will be 0.0)
+
+print(type(0j))
+print(type(1j))
+
+print(type(0.0j))
+print(type(1.1j))
+print(type(1.j))
+print(type(.1j))
+print(type(0e0j))
+print(type(1e100j))
+print(type(3.14e-10j))
+
+print(type(3.141_593j))
+print(type(3.14e-100_000j))

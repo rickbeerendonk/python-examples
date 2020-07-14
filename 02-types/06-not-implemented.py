aa07f3b472f8d0b0c13d6c65b83e2eb608c5be84
Rick Beerendonk <rick@beerendonk.com>
2020-07-14 14:40:15 +0200

    Add "NotImplemented" example.

---
+++ 02-types/06-not-implemented.py
@@ -0,0 +1,7 @@
+# European Union Public License version 1.2
+# Copyright © 2020 Rick Beerendonk
+
+# Methods should return this value if they do not implement 
+# the operation for the operands provided.
+
+print(type(NotImplemented))  # <class 'NotImplementedType'>

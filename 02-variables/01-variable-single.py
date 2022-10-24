5e87f3e00506f6709fe0e12b95bcbe309005ccb9
Rick Beerendonk <rick@beerendonk.com>
2022-10-24 08:52:35 +0200

    Reorder for consistency with other examples in other languages.

---
+++ 02-variables/01-variable-single.py
@@ -0,0 +1,7 @@
+# European Union Public License version 1.2
+# Copyright © 2020 Rick Beerendonk
+
+single = "one two three"
+print(single)
+
+# one two three
--- 03-variables/01-variable-string.py
+++
@@ -1,7 +0,0 @@
-# European Union Public License version 1.2
-# Copyright © 2020 Rick Beerendonk
-
-a_string = "one two three"
-print("A string: " + a_string)
-
-# A string: one two three

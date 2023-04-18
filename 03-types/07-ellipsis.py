8d0a3f531f1c3378418e57495c864fbcbc8eb085
Rick Beerendonk <rick@beerendonk.com>
2023-04-19 00:37:25 +0200

    Improved comments.

--- 03-types/07-ellipsis.py
+++ 03-types/07-ellipsis.py
@@ -1,7 +1,5 @@
 # European Union Public License version 1.2
 # Copyright © 2020 Rick Beerendonk
 
-print(type(...))
-print(type(Ellipsis))
-
-# <class 'ellipsis'>
+print(type(...))       # <class 'ellipsis'>
+print(type(Ellipsis))  # <class 'ellipsis'>

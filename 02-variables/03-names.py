dacd0c1df8e1e9b7ecbfa6ec62f31d70d34a058f
Rick Beerendonk <rick@beerendonk.com>
2023-04-26 12:00:11 +0200

    Add "variables - names" example.

---
+++ 02-variables/03-names.py
@@ -0,0 +1,17 @@
+# European Union Public License version 1.2
+# Copyright © 2023 Rick Beerendonk
+
+# Legal
+
+validvar = "Python";   print(validvar)
+validvar2 = "Python";  print(validvar2)
+valid_var = "Python";  print(valid_var)
+_valid_var = "Python"; print(_valid_var)
+validVar = "Python";   print(validVar)
+VALIDVAR = "Python";   print(VALIDVAR)
+
+# Illegal
+
+# 2invalidvar = "Python"
+# invalid-var = "Python"
+# invalid var = "Python"

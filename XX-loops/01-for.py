974546901b0e00e24ef154bd9ddb894b5662d1ce
Rick Beerendonk <rick@beerendonk.com>
2023-04-29 12:49:56 +0200

    Add "Loops - For" example.

---
+++ XX-loops/01-for.py
@@ -0,0 +1,11 @@
+# European Union Public License version 1.2
+# Copyright © 2023 Rick Beerendonk
+
+for i in range(5):
+    print('Iteration', i)
+
+# Iteration 0
+# Iteration 1
+# Iteration 2
+# Iteration 3
+# Iteration 4

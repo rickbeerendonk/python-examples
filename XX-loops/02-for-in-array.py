951aa82bfffab10def6db68f7d82f33553c5b120
Rick Beerendonk <rick@beerendonk.com>
2023-04-30 12:50:21 +0200

    Add "Loops - For in Array" example.

---
+++ XX-loops/02-for-in-array.py
@@ -0,0 +1,11 @@
+# European Union Public License version 1.2
+# Copyright © 2023 Rick Beerendonk
+
+players = ["Rafael Nadal", "Roger Federer", "Serena Williams"]
+
+for player in players:
+    print("Tennis player:", player)
+
+# Tennis player: Rafael Nadal
+# Tennis player: Roger Federer
+# Tennis player: Serena Williams

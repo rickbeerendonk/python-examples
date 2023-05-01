5b1ce740266c3e589c211096aff46d2e8b86805c
Rick Beerendonk <rick@beerendonk.com>
2023-05-01 12:57:51 +0200

    Add "Loop - While" example.

---
+++ XX-loops/01-while.py
@@ -0,0 +1,16 @@
+# European Union Public License version 1.2
+# Copyright © 2023 Rick Beerendonk
+
+i = 1
+
+while i < 5:
+    print(f"Iteration {i}")
+    i += 1
+
+print("End")
+
+# Iteration 1
+# Iteration 2
+# Iteration 3
+# Iteration 4
+# End

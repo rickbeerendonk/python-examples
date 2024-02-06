eb105a4350c288380ca7e804e6a42fabf25e9ec6
Rick Beerendonk <rick@beerendonk.com>
2024-02-06 17:43:23 +0100

    Add "conversions - to-string" example.

---
+++ 04-conversions/03-to-string.py
@@ -0,0 +1,39 @@
+# European Union Public License version 1.2
+# Copyright © 2024 Rick Beerendonk
+
+# Booleans
+
+print(str(True))  # 'True'
+print(str(False)) # 'False'
+
+# Strings
+
+print(str("false"))  # 'false'
+print(str("true"))   # 'true'
+
+print(str(""))       # ''
+print(str("other"))  # 'other'
+
+# Int
+
+print(str(0))  # '0'
+print(str(1))  # '1'
+
+# Float
+
+print(str(0.0))  # '0.0'
+print(str(1.0))  # '1.0'
+
+# Imaginary
+
+print(str(0j))  # '0j'
+print(str(1j))  # '1j'
+
+# None
+
+print(str(None))  # 'None'
+
+# Ellipsis
+
+print(str(...))       # 'Ellipsis'
+print(str(Ellipsis))  # 'Ellipsis'

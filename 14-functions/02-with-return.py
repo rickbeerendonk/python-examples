af0fc0ef5b00dfa537668bab2b20350e87991b23
Rick Beerendonk <rick@beerendonk.com>
2024-03-28 17:49:30 +0100

    Add "Functions - With return" example.

---
+++ 14-functions/02-with-return.py
@@ -0,0 +1,14 @@
+# European Union Public License version 1.2
+# Copyright © 2024 Rick Beerendonk
+
+def test_string():
+    return "Result"
+
+def test_int():
+    return 123
+
+print(test_string())
+print(test_int())
+
+# Result
+# 123

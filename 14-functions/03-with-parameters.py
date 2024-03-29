28bf04c0b9c591fbc1ac6dae10a5f9b5139dee02
Rick Beerendonk <rick@beerendonk.com>
2024-03-29 17:49:45 +0100

    Add "Functions - With parameters" example.

---
+++ 14-functions/03-with-parameters.py
@@ -0,0 +1,11 @@
+# European Union Public License version 1.2
+# Copyright © 2024 Rick Beerendonk
+
+def test(text, number):
+    print(text)
+    print(number)
+
+test("Hello Python", 12)
+
+# Hello Python
+# 12

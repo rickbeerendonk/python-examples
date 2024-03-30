079517c23d59f0819f4311fa960410b2f115640c
Rick Beerendonk <rick@beerendonk.com>
2024-03-30 17:49:57 +0100

    Add "Functions - Optional parameters" example.

---
+++ 14-functions/04-optional-parameters.py
@@ -0,0 +1,13 @@
+# European Union Public License version 1.2
+# Copyright © 2024 Rick Beerendonk
+
+def test(text, number=1, boolean=True):
+    print(text)
+    print(number)
+    print(boolean)
+
+test("Hello Python", 12)
+
+# Hello Python
+# 12
+# True

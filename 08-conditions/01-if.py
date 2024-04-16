ac26744e0828a0cf1a76d3122bc66fc1905205a1
Rick Beerendonk <rick@beerendonk.com>
2024-04-16 18:40:37 +0200

    Remove incorrect comments.

--- 08-conditions/01-if.py
+++ 08-conditions/01-if.py
@@ -6,10 +6,8 @@ from datetime import datetime
 moment = datetime.now()
 hour = moment.hour
 
-# One line
 if hour < 6 or hour >= 18:
     print("Good night")
 
-# Block
 if 6 <= hour < 18:
     print("Good day")

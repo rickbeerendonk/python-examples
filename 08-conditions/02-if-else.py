cbebe82e9a79d0c4af392b2efcea9196b36d4575
Rick Beerendonk <rick@beerendonk.com>
2024-04-17 18:41:21 +0200

    Remove duplicate example.

--- 08-conditions/02-if-else.py
+++ 08-conditions/02-if-else.py
@@ -6,10 +6,6 @@ from datetime import datetime
 moment = datetime.now()
 hour = moment.hour
 
-# One line
-print("Good day" if 6 <= hour < 18 else "Good night")
-
-# Block
 if 6 <= hour < 18:
     print("Good day")
 else:

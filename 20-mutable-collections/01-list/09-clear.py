a32399cccb3dea06c2e52fb8314b12375e30ed11
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:31:34 +0200

    Add "Mutable Collections - List - Clear" example.

---
+++ 20-mutable-collections/01-list/09-clear.py
@@ -0,0 +1,11 @@
+# European Union Public License version 1.2
+# Copyright © 2025 Rick Beerendonk
+
+lst = ["A", "B", "C"]
+
+lst.clear()  # Removes all items
+
+for item in lst:
+    print(item)
+
+# (no output)

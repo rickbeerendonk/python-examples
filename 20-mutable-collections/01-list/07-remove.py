e7e35c6983fa18c55b4575057fcda6879f47c2a0
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:20:58 +0200

    Add "Mutable Collections - List - Remove" example.

---
+++ 20-mutable-collections/01-list/07-remove.py
@@ -0,0 +1,14 @@
+# European Union Public License version 1.2
+# Copyright © 2025 Rick Beerendonk
+
+lst = ["A", "C", "B", "C", "D"]
+
+lst.remove("C")  # Removes first "C"
+
+for item in lst:
+    print(item)
+
+# A
+# B
+# C
+# D

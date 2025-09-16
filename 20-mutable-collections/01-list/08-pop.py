2aec22fc1e87f6ffe6d8439e192cb3c5382164f6
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:30:43 +0200

    Add "Mutable Collections - List - Pop" example.

---
+++ 20-mutable-collections/01-list/08-pop.py
@@ -0,0 +1,13 @@
+# European Union Public License version 1.2
+# Copyright © 2025 Rick Beerendonk
+
+lst = ["A", "B", "C", "D"]
+
+lst.pop(1)  # Removes the second item
+
+for item in lst:
+    print(item)
+
+# A
+# C
+# D

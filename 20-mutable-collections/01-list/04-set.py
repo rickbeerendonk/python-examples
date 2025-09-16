384a2eb2c8afe4879825fa1a1d36c1220586fe65
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:11:10 +0200

    Add "Mutable Collections - List - Set" example.

---
+++ 20-mutable-collections/01-list/04-set.py
@@ -0,0 +1,15 @@
+# European Union Public License version 1.2
+# Copyright © 2025 Rick Beerendonk
+
+lst = ["A", "B", "C"]
+
+print(lst[1])  # B
+
+lst[1] = "D"  # Change item at index 1
+
+for item in lst:
+    print(item)
+
+# A
+# D
+# C

c39beb88d99c8c6e6d7291dc4cbece4fe5172973
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:15:52 +0200

    Add "Mutable Collections - List - Insert" example.

---
+++ 20-mutable-collections/01-list/06-insert.py
@@ -0,0 +1,14 @@
+# European Union Public License version 1.2
+# Copyright © 2025 Rick Beerendonk
+
+lst = ["A", "B", "C"]
+
+lst.insert(1, "D")  # Inserts "D" at index 1
+
+for item in lst:
+    print(item)
+
+# A
+# D
+# B
+# C

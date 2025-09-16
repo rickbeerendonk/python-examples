80b8d4b537549fc78919a9cc103551a51040468c
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:14:10 +0200

    Add "Mutable Collections - List - Append" example.

---
+++ 20-mutable-collections/01-list/05-append.py
@@ -0,0 +1,14 @@
+# European Union Public License version 1.2
+# Copyright © 2025 Rick Beerendonk
+
+lst = ["A", "B", "C"]
+
+lst.append("D")  # Adds "D" at the end
+
+for item in lst:
+    print(item)
+
+# A
+# B
+# C
+# D

d33bf0afe316226ea715ab000640191e3102d3fd
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 01:18:32 +0200

    Add "lambda - basics" example.

---
+++ 26-lambda/01-basic.py
@@ -0,0 +1,9 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+# lambda is a small anonymous function
+# ...that can take any number of arguments, 
+# ...but can only have one expression.
+square = lambda n: n * n
+
+print(square(5))  # 25

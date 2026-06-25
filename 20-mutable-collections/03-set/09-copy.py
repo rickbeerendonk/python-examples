b8befdf4172ef9ba35b0689e8fb4ca8438b23397
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 01:47:06 +0200

    Add "Mutable Collections - Set - copy" example.

---
+++ 20-mutable-collections/03-set/09-copy.py
@@ -0,0 +1,10 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+st = {"A", "B", "C"}
+
+new_st = st.copy()
+new_st.add("D")
+
+print(len(st))  # 3
+print(len(new_st))  # 4

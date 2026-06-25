4bfda9c20d280b3e07309b938dc6cbf6da6ac691
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 00:50:03 +0200

    Add "Ranges - Step Values" example.

---
+++ 11-ranges/03-step-values.py
@@ -0,0 +1,8 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+values = [1, 2, 3, 4, 5, 6, 7]
+
+# Step values
+print(values[::2])     # [1, 3, 5, 7]
+print(values[1::2])    # [2, 4, 6]

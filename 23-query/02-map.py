955df9f669963c9e5203f6222771d1024a3e43cd
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 01:18:58 +0200

    Add "query - map" example.

---
+++ 23-query/02-map.py
@@ -0,0 +1,15 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+numbers = [1, 2, 3, 4, 5]
+
+squared_numbers = map(lambda n: n * n, numbers)
+
+for number in squared_numbers:
+    print(number)
+
+# 1
+# 4
+# 9
+# 16
+# 25

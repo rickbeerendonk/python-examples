720e9f2e7e74779f8a133def796a4e9790af2beb
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 00:50:09 +0200

    Add "Ranges - Reverse Values" example.

---
+++ 11-ranges/04-reverse-values.py
@@ -0,0 +1,8 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+values = [1, 2, 3, 4, 5, 6, 7]
+
+# Reverse values
+print(values[::-1])    # [7, 6, 5, 4, 3, 2, 1]
+print(values[5:1:-1])  # [6, 5, 4, 3]

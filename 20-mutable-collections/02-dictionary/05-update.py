8beb0d246080e69eb0ef28e5e1d0448e2d7c069d
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 01:22:14 +0200

    Add "Mutable Collections - Dictionary - update" example.

---
+++ 20-mutable-collections/02-dictionary/05-update.py
@@ -0,0 +1,13 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+dct = {"A": 1, "B": 2}
+
+dct.update({"B": 20, "C": 3})
+
+for key, value in dct.items():
+    print(f"{key}={value}")
+
+# A=1
+# B=20
+# C=3

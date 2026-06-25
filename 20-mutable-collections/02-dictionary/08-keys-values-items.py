220cfc14ef4d78ec4ece81dcc761096def5ec58c
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 01:45:21 +0200

    Add "Mutable Collections - Dictionary - Keys Values Items" example.

---
+++ 20-mutable-collections/02-dictionary/08-keys-values-items.py
@@ -0,0 +1,8 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+dct = {"A": 1, "B": 2}
+
+print(list(dct.keys()))  # ['A', 'B']
+print(list(dct.values()))  # [1, 2]
+print(list(dct.items()))  # [('A', 1), ('B', 2)]

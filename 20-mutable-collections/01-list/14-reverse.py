a80556824bab6e431b75afafb113ce9d457187d4
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 00:59:50 +0200

    Add "Mutable Collections - List - Reverse" examples.

---
+++ 20-mutable-collections/01-list/14-reverse.py
@@ -0,0 +1,14 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+lst = ["A", "B", "C", "D"]
+
+lst.reverse()  # Reverses list in place
+
+for item in lst:
+    print(item)
+
+# D
+# C
+# B
+# A

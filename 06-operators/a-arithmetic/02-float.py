99f47313c6e3f76e4b7d04b18be7cc43a8f44391
Rick Beerendonk <rick@beerendonk.com>
2026-06-25 21:23:31 +0200

    Add "Operators - Arithmetic - float" example.

---
+++ 06-operators/a-arithmetic/02-float.py
@@ -0,0 +1,10 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+print(f"float: 7.0 + 2.0 : {7.0 + 2.0} ")  # 9.0  = addition
+print(f"float: 7.0 - 2.0 : {7.0 - 2.0} ")  # 5.0  = subtraction
+print(f"float: 7.0 * 2.0 : {7.0 * 2.0} ")  # 14.0 = multiplication
+print(f"float: 7.0 / 2.0 : {7.0 / 2.0} ")  # 3.5  = true division
+print(f"float: 7.0 // 2.0: {7.0 // 2.0}")  # 3.0  = floor division
+print(f"float: 7.0 % 2.0 : {7.0 % 2.0} ")  # 1.0  = modulus
+print(f"float: 7.0 ** 2.0: {7.0 ** 2.0}")  # 49.0 = exponentiation

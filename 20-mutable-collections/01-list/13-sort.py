055b3c0c5238b5b10fe8a7ef2f8521b453391c23
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 00:59:43 +0200

    Add "Mutable Collections - List - Sort" examples.

---
+++ 20-mutable-collections/01-list/13-sort.py
@@ -0,0 +1,14 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+lst = ["C", "A", "D", "B"]
+
+lst.sort()  # Sorts items ascending
+
+for item in lst:
+    print(item)
+
+# A
+# B
+# C
+# D

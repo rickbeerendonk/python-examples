3ff41fb719dd44d27c412178ca08c11554e610ae
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 01:45:36 +0200

    Add "Mutable Collections - Dictionary - copy" example.

---
+++ 20-mutable-collections/02-dictionary/09-copy.py
@@ -0,0 +1,10 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+dct = {"A": 1, "B": 2}
+
+new_dct = dct.copy()
+new_dct["C"] = 3
+
+print(len(dct))  # 2
+print(len(new_dct))  # 3

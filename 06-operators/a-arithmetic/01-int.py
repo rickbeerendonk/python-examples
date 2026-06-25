e63195b39622893536ff6d1d5e0cbbb64b48e1fc
Rick Beerendonk <rick@beerendonk.com>
2026-06-25 21:23:19 +0200

    Add "Operators - Arithmetic - int" example.

---
+++ 06-operators/a-arithmetic/01-int.py
@@ -0,0 +1,13 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+print(f"int: 7 + 2 : {7 + 2} ")  # 9   = addition
+print(f"int: 7 - 2 : {7 - 2} ")  # 5   = subtraction
+print(f"int: 7 * 2 : {7 * 2} ")  # 14  = multiplication
+print(f"int: 7 / 2 : {7 / 2} ")  # 3.5 = true division
+print(f"int: 7 // 2: {7 // 2}")  # 3   = floor division
+print(f"int: 7 % 2 : {7 % 2} ")  # 1   = modulus
+print(f"int: 7 ** 2: {7 ** 2}")  # 49  = exponentiation
+
+# / always returns a float
+# // returns an int if both operands are ints, otherwise it returns a float.

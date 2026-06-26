fd6715bb32627d9b8e11b4e385b53dec8149da9f
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 09:49:15 +0200

    Add "Lambda - map" example.

---
+++ 26-lambda/09-map.py
@@ -0,0 +1,15 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+numbers = [1, 2, 3, 4, 5]
+
+squares = map(lambda n: n * n, numbers)
+
+for number in squares:
+    print(number)
+
+# 1
+# 4
+# 9
+# 16
+# 25

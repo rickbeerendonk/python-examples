157342d9e57534ad14d2ddf2e1ae0ba3caf3676b
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 09:46:38 +0200

    Add "Query - sorted" example.

---
+++ 23-query/09-sorted.py
@@ -0,0 +1,15 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+numbers = [8, 2, 4, 1, 6]
+
+sorted_numbers = sorted(numbers)
+
+for number in sorted_numbers:
+    print(number)
+
+# 1
+# 2
+# 4
+# 6
+# 8

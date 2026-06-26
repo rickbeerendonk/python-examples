2094e6aee5c9b2d7b33d03feeda4dda2e9489467
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 02:01:36 +0200

    Add "Query - any" example.

---
+++ 23-query/04-any.py
@@ -0,0 +1,8 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+numbers = [1, 3, 5, 8]
+
+has_even_number = any(n % 2 == 0 for n in numbers)
+
+print(has_even_number)  # True

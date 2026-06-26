48651f9740357c2fce91eb111a9d886d04d791ee
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 09:52:53 +0200

    Add "Query - enumerate" example.

---
+++ 23-query/11-enumerate.py
@@ -0,0 +1,11 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+names = ["A", "B", "C"]
+
+for index, name in enumerate(names):
+    print(f"{index}:{name}")
+
+# 0:A
+# 1:B
+# 2:C

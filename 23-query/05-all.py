897a0030a2d3d2d3c65ee41300297dcd53a38f9a
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 02:01:52 +0200

    Add "Query - all" example.

---
+++ 23-query/05-all.py
@@ -0,0 +1,8 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+numbers = [2, 4, 6, 8]
+
+all_even_numbers = all(n % 2 == 0 for n in numbers)
+
+print(all_even_numbers)  # True

54e1c32c039377bc4f5300b528c8d0d1c96a3710
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 09:37:35 +0200

    Add "Immutable Collections - Tuple - create" example.

---
+++ 21-immutable-collections/01-tuple/02-create.py
@@ -0,0 +1,7 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+# tpl = ("A",) # Use comma to create a tuple with one item
+tpl = ("A", "B", "C")
+
+print(len(tpl))  # 3

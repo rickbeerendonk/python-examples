40394360ffc7d8b294e6fb8f8ce855e98acab215
Rick Beerendonk <rick@beerendonk.com>
2026-06-26 09:58:29 +0200

    Add "Query - zip" example.

---
+++ 23-query/12-zip.py
@@ -0,0 +1,12 @@
+# European Union Public License version 1.2
+# Copyright © 2026 Rick Beerendonk
+
+names = ["A", "B", "C"]
+scores = [10, 20, 30]
+
+for name, score in zip(names, scores):
+    print(f"{name}:{score}")
+
+# A:10
+# B:20
+# C:30
